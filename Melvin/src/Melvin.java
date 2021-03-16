8de5dc327f5b1416cadacb8797ccc19d3b09f9cb
politewasp <mi.matson42@gmail.com>
2021-03-16 00:13:55 -0500

    added javadocs comments to Melvin - also testing first commit

--- Melvin/src/Melvin.java
+++ Melvin/src/Melvin.java
@@ -1,3 +1,12 @@
+/**
+ * <h1>Melvin</h1>
+ * Driver class
+ *
+ * @author Maraiah Matson
+ * @version 1.0
+ * @since 2021-03-16
+ */
+
 public class Melvin {
     public static void main(String[] args){
         System.out.println("This is epic");
