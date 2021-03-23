475bcd1e90561402baab9f30e4466ebaaa37c197
politewasp <mi.matson42@gmail.com>
2021-03-23 17:32:46 -0500

    moved GUI implementation out of Driver class. note: make sure to write imports BEFORE javadocs comments to avoid dangling documentation

--- Melvin/src/Melvin.java
+++ Melvin/src/Melvin.java
@@ -6,51 +6,11 @@
  *  @version 1.1
  *  @since 2021-03-16
  */
-
-import javax.swing.*;
-import java.awt.*;
-
-public class Melvin extends JFrame{
-    JButton createButton = new JButton("+");
-    GridLayout layout = new GridLayout(0,1,5,5);
-    //TODO temporary until we have a storage system
-    static Goal[] goals = {new Goal("Goal 1"), new Goal("Goal 2"), new Goal("Goal 3")};
-
-    public Melvin(){
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
-        this.setLocationRelativeTo(null);
-        this.setTitle("Badger");
-        this.setLayout(layout);
-        for(Goal g:goals){
-            this.add(new goalView(g));
-        }
-        this.setVisible(true);
-
+final class Melvin{
+    private Melvin(){
+        // adding this method prevents instantiation of Melvin elsewhere as a safeguard
     }
-
     public static void main(String[] args) {
-        new Melvin();
-    }
-}
-class goalView extends JPanel{
-    JLabel goalName = new JLabel();
-    JLabel goalCat = new JLabel();
-    JLabel status = new JLabel();
-
-    //Eventually replace with constructor that builds panel from a goal
-    public goalView(Goal goal){
-        this.setBorder(BorderFactory.createLineBorder(Color.black));
-        this.add(goalName);
-        goalName.setText(goal.getName());
-        goalName.setFont(new Font(goalName.getFont().getName(), Font.PLAIN, 20));
-        this.add(new JLabel("in category"));
-        this.add(goalCat);
-        goalCat.setText(goal.getCategoryName());
-        goalCat.setFont(new Font(goalCat.getFont().getName(), Font.BOLD, 14));
-        this.add(status);
-        status.setText("Not yet logged!");
-
-
+        new Window();
     }
 }
