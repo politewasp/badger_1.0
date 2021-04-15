130c4cf30685f1423fef3f387527943a9aed048e
Emily <emily.s.wirth@gmail.com>
2021-04-14 19:11:08 -0500

    added logic for if start is within month and if start is not within target month

--- Melvin/src/DateManagement.java
+++ Melvin/src/DateManagement.java
@@ -6,18 +6,15 @@ import java.text.ParseException;
 /**
  * This class is in charge of creating an array of date instances that a goal checkin happens
  */
-public class DateManagement
-{
+public class DateManagement {
 
     /**
-     *
-     * @param Goal Goal from goal
+     * @param Goal  Goal from goal
      * @param month target month for dates
-     * @param year target year
+     * @param year  target year
      * @return list of check in dates within month
      */
-    public ArrayList<Calendar> getMonth(Goal Goal, int month, int year)
-    {
+    public ArrayList<Calendar> getMonth(Goal Goal, int month, int year) {
         ArrayList<Calendar> dates = new ArrayList<>();
         ArrayList<Integer> daysOfWeek = Goal.getDaysOfWeek();
 
@@ -25,25 +22,36 @@ public class DateManagement
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar c = Calendar.getInstance();
         Calendar limit = Calendar.getInstance();
-        limit.set(year, month +1, 1);
-        try{
+        Calendar start = Calendar.getInstance();
+
+        limit.set(year, month + 1, 1);
+        start.set(year, month, 1);
+        try {
             //sets first date to the first ever date
+
             c.setTime(sdf.parse(Goal.getStart()));
-        }catch(ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
         }
-        for (int day: daysOfWeek)
-        {
-            c.set(Calendar.DAY_OF_WEEK, day);
-            while(c.before(limit))
-            {
-                dates.add(c);
-                c.add(Calendar.WEEK_OF_MONTH, 1);
+        if (c.before(start)) {
+            c = start;
+            for (int day : daysOfWeek) {
+                c.set(Calendar.DAY_OF_WEEK, day);
+                while (c.before(limit)) {
+                    dates.add(c);
+                    c.add(Calendar.WEEK_OF_MONTH, 1);
+                }
             }
-
         }
-    return dates;
-
+        else if(c.after(start) && c.before(limit)) {
+            for (int day : daysOfWeek) {
+                c.set(Calendar.DAY_OF_WEEK, day);
+                while (c.before(limit)) {
+                    dates.add(c);
+                    c.add(Calendar.WEEK_OF_MONTH, 1);
+                }
+            }
+        }
+        return dates;
     }
-
 }
