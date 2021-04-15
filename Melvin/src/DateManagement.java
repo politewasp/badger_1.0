dad8329a1b23aa9dda1135ab06ac4c6645f4af6a
Emily <emily.s.wirth@gmail.com>
2021-04-14 19:00:35 -0500

    created getMonth makes a list of calendar dates

--- Melvin/src/DateManagement.java
+++ Melvin/src/DateManagement.java
@@ -9,10 +9,33 @@ import java.text.ParseException;
 public class DateManagement
 {
 
-    public ArrayList<String> getMonth(Goal goal, int Month, int Year)
+    public ArrayList<Calendar> getMonth(Goal Goal, int month, int year)
     {
-        ArrayList<String> dates = new ArrayList<>();
-        ArrayList<Integer> daysOfWeek = goal.getDaysOfWeek();
+        ArrayList<Calendar> dates = new ArrayList<>();
+        ArrayList<Integer> daysOfWeek = Goal.getDaysOfWeek();
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        Calendar limit = Calendar.getInstance();
+        limit.set(year, month +1, 1);
+        try{
+            //sets first date to the first ever date
+            c.setTime(sdf.parse(Goal.getStart()));
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        for (int day: daysOfWeek)
+        {
+            c.set(Calendar.DAY_OF_WEEK, day);
+            while(c.before(limit))
+            {
+                dates.add(c);
+                c.add(Calendar.WEEK_OF_MONTH, 1);
+            }
+
+        }
+    return dates;
 
     }
 
